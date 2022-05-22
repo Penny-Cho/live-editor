57dfde10c057b5845dbc6c078e2ad4994e3bf573
Penny-Cho <tinysalmon0@gmail.com>
2022-05-22 09:35:24 +0900

    added iframe to work

---
+++ src/plugins/fetch-plugin.ts
@@ -0,0 +1,73 @@
+import axios from "axios";
+import * as esbuild from "esbuild-wasm";
+import localForage from "localforage";
+
+// save npm packages in the indexedDB so that not have to re-download it again
+const fileCache = localForage.createInstance({
+  name: "filecache",
+});
+
+export const fetchPlugin = (inputCode: string) => {
+  return {
+    name: "fetch-plugin",
+    setup(build: esbuild.PluginBuild) {
+      // jsx type
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        // check if we have already downloaded the file,
+        //         if it is, it should be in the cache.
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        // // if it is ithe cache, doesn't have to re-download
+        if (cachedResult) {
+          return cachedResult;
+        }
+      });
+
+      // css type
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+        const contents = `
+            const style = document.createElement("style");
+            style.innerText ='${escaped}';
+            document.head.appendChild(style);
+        `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+    },
+  };
+};
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,71 +1,30 @@
-import axios from "axios";
 import * as esbuild from "esbuild-wasm";
-import localForage from "localforage";
-
-// save npm packages in the indexedDB so that not have to re-download it again
-const fileCache = localForage.createInstance({
-  name: "filecache",
-});
 
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
-    setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResolve", args);
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
 
-        if (args.path.includes("./" || args.path.includes("../"))) {
-          return {
-            namespace: "a",
-            path: new URL(
-              args.path,
-              "https://unpkg.com" + args.resolveDir + "/"
-            ).href,
-          };
-        }
+    setup(build: esbuild.PluginBuild) {
+      // handling root file index.js
+      build.onResolve({ filter: /(^index\.js)$/ }, () => {
+        return { path: "index.js", namespace: "a" };
+      });
+
+      // handling relative path ./ or ../
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: "a",
+          path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/")
+            .href,
+        };
+      });
+
+      // handling main file of a moudle
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
         };
-      });
-
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
-
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: `
-              const message = require("react");
-              console.log(message);
-            `,
-          };
-        }
-
-        // check if we have already downloaded the file,
-        // if it is, it should be in the cache.
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-
-        // if it is ithe cache, doesn't have to re-download
-        if (cachedResult) {
-          return cachedResult;
-        }
-
-        const { data, request } = await axios.get(args.path);
-        //if it is the first tiem, store the response in cache
-
-        const result: esbuild.OnLoadResult = {
-          loader: "jsx",
-          contents: data,
-          resolveDir: new URL("./", request.responseURL).pathname,
-        };
-
-        await fileCache.setItem(args.path, result);
       });
     },
   };
