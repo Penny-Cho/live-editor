9006ab21c58733edd23fee5dda11eff4a8989b95
Penny-Cho <tinysalmon0@gmail.com>
2022-06-04 10:58:31 +0900

    MDEditor focus toggle editor or preview mode

--- src/components/text-editor/text-editor.tsx
+++ src/components/text-editor/text-editor.tsx
@@ -1,5 +1,44 @@
+import MDEditor from "@uiw/react-md-editor";
+import "./text-editor.css";
+import React, { useEffect, useRef, useState } from "react";
+
 const TextEditor: React.FC = () => {
-  return <div>efw</div>;
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState("");
+
+  // click event로 특정 구역 제ㅣㅏ기
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v!)} />
+      </div>
+    );
+  }
+
+  return (
+    <div className="container" onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={"# header"} />
+    </div>
+  );
 };
 
 export default TextEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import "bulmaswatch/darkly/bulmaswatch.min.css";
 import { createRoot } from "react-dom/client";
-import CodeCell from "./components/code-cell";
+import TextEditor from "./components/text-editor/text-editor";
+// import CodeCell from "./components/code-cell";
 
 const App = () => {
   return (
     <div
       style={{ padding: "3%", minHeight: "100vh", backgroundColor: "#343642" }}
     >
-      <CodeCell />
+      <TextEditor />
     </div>
   );
 };
