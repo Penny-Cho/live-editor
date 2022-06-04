e03626a993b10b6fdd863d1e5d512bf5ef547a22
Penny-Cho <tinysalmon0@gmail.com>
2022-06-04 10:59:45 +0900

    changed to onblur

--- src/components/text-editor/text-editor.tsx
+++ src/components/text-editor/text-editor.tsx
@@ -1,35 +1,46 @@
 import MDEditor from "@uiw/react-md-editor";
 import "./text-editor.css";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 
 const TextEditor: React.FC = () => {
-  const ref = useRef<HTMLDivElement | null>(null);
+  // const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
   const [value, setValue] = useState("");
 
   // click event로 특정 구역 제ㅣㅏ기
-  useEffect(() => {
-    const listener = (event: MouseEvent) => {
-      if (
-        ref.current &&
-        event.target &&
-        ref.current.contains(event.target as Node)
-      ) {
-        return;
-      }
-      setEditing(false);
-    };
-    document.addEventListener("click", listener, { capture: true });
+  // useEffect(() => {
+  //   const listener = (event: MouseEvent) => {
+  //     if (
+  //       ref.current &&
+  //       event.target &&
+  //       ref.current.contains(event.target as Node)
+  //     ) {
+  //       return;
+  //     }
+  //     setEditing(false);
+  //   };
+  //   document.addEventListener("click", listener, { capture: true });
 
-    return () => {
-      document.removeEventListener("click", listener, { capture: true });
-    };
-  }, []);
+  //   return () => {
+  //     document.removeEventListener("click", listener, { capture: true });
+  //   };
+  // }, []);
+
+  // onBlur로 해결
+  const onBlur = (event: React.FocusEvent<HTMLDivElement>): void => {
+    if (value) {
+      setEditing(false);
+    }
+  };
 
   if (editing) {
     return (
-      <div ref={ref}>
-        <MDEditor value={value} onChange={(v) => setValue(v!)} />
+      <div>
+        <MDEditor
+          onBlur={onBlur}
+          value={value}
+          onChange={(v) => setValue(v!)}
+        />
       </div>
     );
   }
