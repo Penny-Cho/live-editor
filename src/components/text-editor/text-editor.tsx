a5c4ce7ce84791f9c4dcb678bc0934086c35d8b0
Penny-Cho <tinysalmon0@gmail.com>
2022-06-05 12:12:03 +0900

    added redux skaffold

--- src/components/text-editor/text-editor.tsx
+++ src/components/text-editor/text-editor.tsx
@@ -1,53 +1,48 @@
 import MDEditor from "@uiw/react-md-editor";
 import "./text-editor.css";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const TextEditor: React.FC = () => {
-  // const ref = useRef<HTMLDivElement | null>(null);
+  const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
   const [value, setValue] = useState("");
 
-  // click event로 특정 구역 제ㅣㅏ기
-  // useEffect(() => {
-  //   const listener = (event: MouseEvent) => {
-  //     if (
-  //       ref.current &&
-  //       event.target &&
-  //       ref.current.contains(event.target as Node)
-  //     ) {
-  //       return;
-  //     }
-  //     setEditing(false);
-  //   };
-  //   document.addEventListener("click", listener, { capture: true });
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
 
-  //   return () => {
-  //     document.removeEventListener("click", listener, { capture: true });
-  //   };
-  // }, []);
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
 
   // onBlur로 해결
-  const onBlur = (event: React.FocusEvent<HTMLDivElement>): void => {
-    if (value) {
-      setEditing(false);
-    }
-  };
+  // const onBlur = (event: React.FocusEvent<HTMLDivElement>): void => {
+  //   if (value) {
+  //     setEditing(false);
+  //   }
+  // };
 
   if (editing) {
     return (
-      <div>
-        <MDEditor
-          onBlur={onBlur}
-          value={value}
-          onChange={(v) => setValue(v!)}
-        />
+      <div ref={ref} className="text-editor">
+        <MDEditor value={value} onChange={(v) => setValue(v || "")} />
       </div>
     );
   }
 
   return (
-    <div className="container" onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={"# header"} />
+    <div className="text-editor " onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={value} />
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import "bulmaswatch/darkly/bulmaswatch.min.css";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./state";
 import TextEditor from "./components/text-editor/text-editor";
 // import CodeCell from "./components/code-cell";
 
 const App = () => {
   return (
-    <div
-      style={{ padding: "3%", minHeight: "100vh", backgroundColor: "#343642" }}
-    >
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div
+        style={{
+          padding: "3%",
+          minHeight: "100vh",
+          backgroundColor: "#343642",
+        }}
+      >
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
